dc4a8eeff1fa6ec990ff84f7bf4a879aa3b40dcc
Clément Mercier <103317008+Traibot@users.noreply.github.com>
2022-11-14 10:26:35 +0100

    Delete Study Cases directory

--- Study Cases/industrial-machine-anomaly-detection/install_cmake.sh
+++
@@ -1,7 +0,0 @@
-apt install build-essential libssl-dev
-wget https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2.tar.gz
-tar -zxvf cmake-3.20.2.tar.gz
-cd cmake-3.20.2
-./bootstrap
-make 
-make install
