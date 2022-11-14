9ea3a179626473db38541e0d7cf45be9437f9486
Clément Mercier <103317008+Traibot@users.noreply.github.com>
2022-11-14 10:14:53 +0100

    Add files via upload

---
+++ Study Cases/industrial-machine-anomaly-detection/install_cmake.sh
@@ -0,0 +1,7 @@
+apt install build-essential libssl-dev
+wget https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2.tar.gz
+tar -zxvf cmake-3.20.2.tar.gz
+cd cmake-3.20.2
+./bootstrap
+make 
+make install
