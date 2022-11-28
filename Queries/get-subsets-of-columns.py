66b65f22ef67ce210a84dd3e7f03853224b7a018
Clément Mercier <103317008+Traibot@users.noreply.github.com>
2022-11-28 11:29:19 +0100

    Update get-subsets-of-columns.py

--- Queries/get-subsets-of-columns.py
+++ Queries/get-subsets-of-columns.py
@@ -10,6 +10,8 @@ session = sh.sandbox()
 
 data = session.load_test_data()
 
+data = data.add_column("Sepal_Area", lambda row: row.Sepal_Length*row.Sepal_Width)
+
 data_subset = data.select_columns(['Sepal_Area','Sepal_Length','Sepal_Width'])
 
 # or 
