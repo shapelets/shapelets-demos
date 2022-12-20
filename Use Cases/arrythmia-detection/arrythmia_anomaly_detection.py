3e50351a4ab0ccbc7d0225ebfe1970420a383324
adrian-carrio <86968261+adrian-carrio@users.noreply.github.com>
2022-12-20 15:36:04 +0100

    Anomaly detection dataapp

---
+++ Use Cases/arrythmia-detection/arrythmia_anomaly_detection.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2022 Grumpy Cat Software S.L.
+#
+# This Source Code is licensed under the MIT 2.0 license.
+# the terms can be found in LICENSE.md at the root of
+# this project, or at http://mozilla.org/MPL/2.0/.
+
+import pandas as pd
+from shapelets.apps import DataApp
+from shapelets.apps.widgets import LineChart, View
+import stumpy
+import numpy as np
+import datetime
+
+def computeAndPlotAnomalies(ts: pd.Series, windowSize: int, top_k: int) -> LineChart:
+    # Find discords
+    mp = stumpy.stump(ts, windowSize)
+
+    discords_idx = np.flip(np.argsort(mp[:, 0]))
+
+    views = []
+    for m in discords_idx[0:top_k]:
+        views.append(View(start=ts.index[m], end=ts.index[m]+datetime.timedelta(milliseconds=windowSize)))
+    return LineChart(title='Anomalies', data=ts, views=views)
+
+app = DataApp(name="Anomaly detection",
+              version=(1,0),
+              description="In this app, Data from the MIT-BIH Arrhythmia Database (mitdb) are analyzed looking for premature ventricular contractions (PVC).")
+
+df = pd.read_csv('Resources/mitdb102.csv', header=None, index_col=0, names=['MLII', 'V1'], skiprows=200000, nrows=20000)
+
+df.index = pd.to_datetime(df.index, unit='s')
+
+df = df.resample('1ms').asfreq().fillna(method='ffill')
+
+windowSize = app.number_input(title="Window size value [ms]", value=250)
+
+app.place(windowSize, width=6)
+
+top_k = app.slider(title="Desired number of anomalies: ", min_value=1, max_value=20, step=1, value=1)
+
+app.place(top_k, width=6)
+
+button = app.button("Execute anomaly-detection")
+
+app.place(button)
+
+line_chart1 = app.line_chart(title='MLII', data=df['MLII'])
+
+app.place(line_chart1)
+
+line_chart1.bind(computeAndPlotAnomalies, df['MLII'], windowSize, top_k, triggers=[button])
+
+app.register()
