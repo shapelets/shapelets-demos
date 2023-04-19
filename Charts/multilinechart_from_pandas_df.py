0def7e2ada329a8063b7a6847a97daf60487ad10
carlosevi94 <carlosevillabarcelo@gmail.com>
2023-04-19 17:27:06 +0200

    Example multi linechart

---
+++ Charts/multilinechart_from_pandas_df.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2023 Grumpy Cat Software S.L.
+#
+# This Source Code is licensed under the MIT 2.0 license.
+# the terms can be found in LICENSE.md at the root of
+# this project, or at http://mozilla.org/MPL/2.0/.
+
+from shapelets.apps import DataApp
+import pandas as pd
+from vega_datasets import data
+
+# Instantiate DataApp
+app = DataApp("multilinechart_from_pandas_df")
+
+# Get sample data frame
+source = data.cars()
+
+# Create line chart widget and plot df
+lc = app.line_chart(data=source, title="My title")
+
+# Place line chart widget
+app.place(lc)
+
+# Register data app
+app.register()
