2d2a06f1f8d34d4aa67dd5d839f9fafe10ac012f
carlosevi94 <carlosevillabarcelo@gmail.com>
2023-07-13 12:43:34 +0200

    Script for create users

---
+++ create_users.py
@@ -0,0 +1,7 @@
+import shapelets as sh
+
+sh.register(user_name="admin",password="admin") 
+sh.register(user_name="user1",password="user1") 
+sh.register(user_name="user2",password="user2") 
+sh.register(user_name="user3",password="user3") 
+
